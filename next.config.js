f030b375a3a455ea5314f3f61cdd110ddd79bd62
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-07-21 12:41:31 +0200

    feat(next.config): configure Next.js to work with xrpl.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,46 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (
+    config,
+    { buildId, defaultLoaders, dev, isServer, nextRuntime, webpack },
+  ) => {
+    const fallback = config.resolve.fallback || {};
+
+    Object.assign(fallback, {
+      assert: require.resolve('assert'),
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify'),
+      stream: require.resolve('stream-browserify'),
+      url: require.resolve('url'),
+      ws: require.resolve('xrpl/dist/npm/client/WSWrapper'),
+    });
+
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser',
+      }),
+    ]);
+
+    const aliases = config.resolve.alias || {};
+
+    Object.assign(aliases, {
+      ws: path.resolve(
+        __dirname,
+        './node_nodules/xrpl/dist/npm/client/WSWrapper.js',
+      ),
+    });
+
+    config.resolve.alias = aliases;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
