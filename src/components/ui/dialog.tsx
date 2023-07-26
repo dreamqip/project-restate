7b2fb98c501e23b4ba651beccaa0d00bcf165e5a
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-07-26 12:35:08 +0200

    fix(ui/dialog): change overlay opacity to 10 percent

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -23,7 +23,7 @@ const DialogOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     className={cn(
-      'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 overflow-y-auto bg-background/80 backdrop-blur-xl',
+      'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 overflow-y-auto bg-background/10 backdrop-blur-xl',
       className,
     )}
     ref={ref}
