4c80198525f43f5a7c5244b8d856c9c6e39e9a4a
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-07-26 12:30:24 +0200

    refactor(wallet): remove mnemonics provider as it was moved to root layout

--- src/app/wallet/layout.tsx
+++ src/app/wallet/layout.tsx
@@ -1,13 +1,8 @@
-import { MnemonicsProvider } from '@/providers/mnemonics-provider';
 
 export default function WalletLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <main className='p-32'>
-      <MnemonicsProvider>{children}</MnemonicsProvider>
-    </main>
-  );
+  return <main className='p-32'>{children}</main>;
 }
