3b46bb0aa44264fd4eaa60c2b0a2dd41785fc746
jusikXL <jusikxl@gmail.com>
2023-07-29 22:49:02 +0200

    refactor(mint-nft-modal): test mint properties

--- src/components/mint-nft-modal.tsx
+++ src/components/mint-nft-modal.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-import type { Product } from '@/app/marketplace/test-product';
-
 import {
   Button,
   Dialog,
@@ -17,20 +13,16 @@ import { useWallet } from '@/hooks/use-wallet';
 import { DialogClose } from '@radix-ui/react-dialog';
 import { ChevronLeftIcon } from 'lucide-react';
 
-interface MintNftModalProps {
-  product: Product;
-}
-export default function MintNftModal({ product }: MintNftModalProps) {
+export default function MintNftModal() {
   const { mintNFT } = useLedger();
   const { wallet } = useWallet();
 
   const onSubmit = async () => {
-    //TODO: add loader
     try {
       const response = await mintNFT({
         flags: 8,
-        NFTokenTaxon: 1,
-        transferFee: 500,
+        NFTokenTaxon: 0,
+        transferFee: 5000,
       });
       console.log(response);
     } catch (error) {
@@ -49,10 +41,10 @@ export default function MintNftModal({ product }: MintNftModalProps) {
         <DialogHeader>
           <DialogClose className='mb-2 flex max-w-min items-center justify-start whitespace-nowrap text-accents-3'>
             <ChevronLeftIcon className='mr-1 inline-block h-6 w-6' />
-            Back to {product.name}
+            Back
           </DialogClose>
           <DialogTitle className='!mb-3 !mt-0 text-left text-3xl font-bold'>
-            {product.name}
+            Test Mint
           </DialogTitle>
           <DialogDescription className='text-left text-base'>
             The result is in console
